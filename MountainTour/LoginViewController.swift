6bd5f4057c243e29c84b28e81b1e347ceecfa41b
Roberto Santos <robot-o@hotmail.com>
2019-02-16 23:03:31 -0800

    troubleshoot navigation controller

--- MountainTour/LoginViewController.swift
+++ MountainTour/LoginViewController.swift
@@ -10,7 +10,34 @@ import Foundation
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var userNameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        navigateToMainInterface()
+        
+    }
+    
+    private func navigateToMainInterface() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        guard let mainNavigationVC = mainStoryboard.instantiateViewController(withIdentifier: "MainNavigationController") as?
+            MainNavigationController else {
+                return
+        }
+        
+        print(mainNavigationVC.topViewController!)
+        print(mainNavigationVC.viewControllers)
+        
+        if let mainVC = mainNavigationVC.topViewController as? ViewController {
+            mainVC.userName = userNameField.text
+            mainVC.password = passwordField.text
+        }
+        
+        present(mainNavigationVC, animated: true, completion: nil)
     }
 }
--- MountainTour/ViewController.swift
+++ MountainTour/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userName: String?
+    var password: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(userName!)
+        print(password!)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
     }
 
 
