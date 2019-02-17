642841bce9aa68f547a3c433630f9fe8b2eb3ace
Roberto Santos <robot-o@hotmail.com>
2019-02-16 16:21:24 -0800

    initialize repository and set up file structure

---
+++ MountainTour/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  MountainTour
+//
+//  Created by Roberto Santos on 2/16/19.
+//  Copyright © 2019 Roberto Santos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ MountainTour/MainNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  MainNavigationController.swift
+//  MountainTour
+//
+//  Created by Roberto Santos on 2/16/19.
+//  Copyright © 2019 Roberto Santos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
