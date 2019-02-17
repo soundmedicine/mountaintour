fdce0ec65895437b77db784b83aa79f7351b83da
Roberto Santos <robot-o@hotmail.com>
2019-02-17 10:40:47 -0800

    troubleshoot layout on hut view

--- MountainTour/ViewController.swift
+++ MountainTour/ViewController.swift
@@ -15,15 +15,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(userName!)
-        print(password!)
+        print("Username is \(userName)")
+        print("Password is \(password)")
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        //Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        //Dispose of any resources that can be recreated.
+//    }
 
 
 }
